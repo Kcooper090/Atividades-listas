d878b26801798cb3e9ec871d21d3280ded9040ed
Kcooper090 <k03342317@gmail.com>
2026-04-07 17:45:03 -0400

    Add files via upload

---
+++ Automóvel.py
@@ -0,0 +1,4 @@
+distancia = int(input("Qual foi a distância percorrida em quilometros?"))
+Combustível_gasto = int(input("Qual foi a quantidade em litros de combustível gasta?"))
+consumo_medio_auto = distancia / Combustível_gasto
+print(f" O custo médio de um automóvel é {consumo_medio_auto} reais.")

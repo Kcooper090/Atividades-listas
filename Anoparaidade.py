991d433b8f46d9a9c705dc1a740d71da48d2e724
Kcooper090 <k03342317@gmail.com>
2026-04-07 17:08:20 -0400

    Add files via upload

---
+++ Anoparaidade.py
@@ -0,0 +1,7 @@
+nome = input("Escreva seu nome:")
+idade = int(input("Digite sua idade:"))
+dias = idade * 365
+print(f"{nome} tem {dias} dias de vida!")
+mes= 30
+idade_mes = dias / mes
+print(f" e convertendo em meses. {idade_mes} meses de vida!" )

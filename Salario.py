e06242fdca4c8e45d2f89fc59cb74512310cc453
Kcooper090 <k03342317@gmail.com>
2026-04-07 17:24:41 -0400

    Add files via upload

---
+++ Salario.py
@@ -0,0 +1,4 @@
+
+salario = int(input("Qual é a sua renda mensal fornecida pelo empresa?"))
+salario_atual = (salario * 0.25) + salario
+print(f"Seu salario, com a almento de 25% é igual a {salario_atual}")

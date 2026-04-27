4c00658dd23b18512f01c5111a5acc0a7a919f70
Kcooper090 <k03342317@gmail.com>
2026-04-27 17:03:06 -0400

    Add files via upload

---
+++ media6.py
@@ -0,0 +1,9 @@
+ 
+for c in range (5):
+    nome = input("Insira seu nome ")
+    nota_1 = float(input("Insira a primeira nota "))
+    nota_2 = float(input("Insira sua segunda nota "))
+    
+    média = (nota_1 + nota_2) / 2
+    print(f"{nome} sua média foi {média}")
+
